ecc5440e1a5a410bf837a68c675c8bfe461281e4
Christian Thorby <christianthorby@hotmail.com>
2019-02-20 20:10:24 +0100

    change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,13 @@
 # Modify this file to make it your own :-)
 
 variable "app_prefix" {
-  default = "pgr301-zzz"
+  default = "pgr301-exam"
 }
 
 variable "pipeline_name" {
-  default = "pgr301-your-initialsz"
+  default = "pgr301-pipe"
+}
+
+variable "heroku_email" {
+  default = "anonymeksamen123@gmail.com"
 }
